1834f256c1bc3e0273029759004da2c272a89bb0
Joanna <m.joanna.foss@gmail.com>
2022-04-09 00:32:47 -0500

    working on animate function

--- script.js
+++ script.js
@@ -3,6 +3,25 @@
     const c = canvas.getContext("2d")
 
     initialize()
+    animate()
+
+    window.addEventListener('keydown', (event) => {
+        let key = event.key
+        switch(key) {
+            case "ArrowUp": 
+                console.log("Pressed Up Key")
+                break
+            case "ArrowRight":
+                console.log("Pressed Right Key")
+                break
+            case "ArrowDown":
+                console.log("Pressed Down Key")
+                break
+            case "ArrowLeft":
+                console.log("Pressed Left Key")
+                break
+        }
+    })
 
     function initialize() {
         window.addEventListener('resize', resizeCanvas, false)
@@ -34,9 +53,22 @@
             c.fillRect(0, 0, canvas.width, canvas.height)
             c.drawImage(background_image, -1640, -760)
             c.drawImage(player_image,
-                canvas.width/2 - player_image.width/2,
-                canvas.height/2 - player_image.height/2)
+                0,
+                0,
+                player_image.width/4,
+                player_image.height,
+                canvas.width/2 - (player_image.width/4)/2,
+                canvas.height/2 - (player_image.height/4)/2,
+                player_image.width/4,
+                player_image.height)
         }
+    }
+
+    function animate() {
+        if(true) {
+            window.requestAnimationFrame(animate)
+        }
+        console.log("animating")
     }
 
 })();
